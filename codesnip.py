a910dd8b72131f3b91959dd81bc9899370e4e5b7
alexb821 <alex.b821@gmail.com>
2025-02-03 15:27:13 -0800

    Update codesnip.py

--- codesnip.py
+++ codesnip.py
@@ -8,3 +8,7 @@ df['col1'] = np.where(df1['col2'] = 'string', 'VALUE', 'OTHERVALUE')
 # you can then use the map to update values in another dataframe whe id matches
 value_map = dict(zip(df1['id'], df1['name']))
 df2['name'] = df2['id'].map(value_map)
+
+
+df1[df1['col1'].str.contains('test', na=False)]
+
