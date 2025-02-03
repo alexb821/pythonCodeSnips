ec8203f3855f72fb7973926c0fa75a8efa20057c
alexb821 <alex.b821@gmail.com>
2025-02-03 10:26:16 -0800

    Update codesnip.py

--- codesnip.py
+++ codesnip.py
@@ -1,3 +1,10 @@
 from copy import deepcopy
 myCopy = deepcopy(myDict)
 
+# if col2 is 'string' then set col1 to VALUE, otherwize set to OTHERVALUE
+df['col1'] = np.where(df1['col2'] = 'string', 'VALUE', 'OTHERVALUE')
+
+# create a dictionary with key value pairs from dataframe values
+# you can then use the map to update values in another dataframe whe id matches
+value_map = dict(zip(df1['id'], df1['name']))
+df2['name'] = df2['id'].map(value_map)
